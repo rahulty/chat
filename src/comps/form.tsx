4bfbb06dc78f9cfbfbe798a8a423bff8e261cbd9
rahulty <69441442+rahulty@users.noreply.github.com>
2021-10-24 22:34:16 +0530

    initial commit

--- src/comps/form.tsx
+++ src/comps/form.tsx
@@ -1,10 +1,15 @@
-export function Form({ onSubmit, children, ...otherProps }) {
+export function Form({ onSubmit, children, autocomplete, ...otherProps }) {
   function onS(e) {
     e.preventDefault();
-    return onSubmit(e);
+    const txtInputs: NodeListOf<HTMLInputElement> = e.target.querySelectorAll(
+      'input[type="text"],input[type="password"]'
+    );
+    const retValue = onSubmit(e);
+    txtInputs.forEach((t) => (t.value = ""));
+    return retValue;
   }
   return (
-    <form onSubmit={onS} {...otherProps}>
+    <form autocomplete={autocomplete || "off"} onSubmit={onS} {...otherProps}>
       {children}
     </form>
   );
--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -3,9 +3,17 @@ import { LoginForm } from "../comps/login-form";
 
 export function LoginPage() {
   return (
-    <div>
-      <LoginForm />
-      <SignupForm />
+    <div id="loginPage">
+      <div id="loginNavbar">
+        <div>Login</div>
+        <div>Sign Up</div>
+      </div>
+      <div class="page">
+        <LoginForm />
+      </div>
+      <div class="page">
+        <SignupForm />
+      </div>
     </div>
   );
 }
